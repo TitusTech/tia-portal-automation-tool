0f3e8c34e968b061ca3c61756c4ac3382f879df3
Wineff Chen Zhen Y. Gulane <wineffg@titusgt.com>
2024-11-18 16:44:17 +0900

    updated network service

--- modules/api.py
+++ modules/api.py
@@ -166,30 +166,50 @@ def create_modules(device_data: dict[str, Any], device: Siemens.Engineering.HW.D
     return
 
 
+def find_network_interface_of_device(imports: Imports, device: Siemens.Engineering.HW.Device) -> list[tuple[Siemens.Engineering.HW.Features.NetworkInterface, Siemens.Engineering.HW.DeviceItem]]:
+    SE: Siemens.Engineering = imports.DLL
+
+    logging.debug(f"Looking for NetworkInterfaces for Device {device.Name}")
+
+    device_items: Siemens.Engineering.HW.DeviceItem = device.DeviceItems[1].DeviceItems # DeviceItems[1] is used because index 0 is a rack / rail
+    network_services: list[Siemens.Engineering.HW.Features.NetworkInterface] = []
+    for i, item in enumerate(device_items):
+        logging.debug(f"Checking if [{i}] DeviceItem {item.Name} is a NetworkInterface")
+
+        network_service: Siemens.Engineering.HW.Features.NetworkInterface = SE.IEngineeringServiceProvider(item).GetService[SE.HW.Features.NetworkInterface]()
+        if not network_service:
+            logging.debug(f"[{i}] DeviceItem {item.Name} is not a NetworkInterface")
+            continue
+
+        logging.debug(f"Found NetworkInterface for DeviceItem {item.Name} at index {i}")
+
+        network_services.append((item, network_service))
+
+    logging.debug(f"Found {len(network_services)} NetworkInterfaces for Device {device.Name}")
+
+    return network_services
 
 
 def create_device_network_service(imports: Imports, device_data: dict[str, Any], device: Siemens.Engineering.HW.Device) -> list[Siemens.Engineering.HW.Features.NetworkInterface]:
     SE: Siemens.Engineering = imports.DLL
 
     interfaces: list[Siemens.Engineering.HW.Features.NetworkInterface] = []
-
-    device_items: Siemens.Engineering.HW.DeviceItem = device.DeviceItems[1].DeviceItems
-    for deviceItem in device_items:
-        logging.debug(f"Accessing a NetworkInterface at DeviceItem {deviceItem.Name}")
-
-        network_service: Siemens.Engineering.HW.Features.NetworkInterface = SE.IEngineeringServiceProvider(deviceItem).GetService[SE.HW.Features.NetworkInterface]()
-        if not network_service:
-
-            logging.debug(f"No NetworkInterface found for DeviceItem {deviceItem.Name}")
-
-        logging.debug(f"Found NetworkInterface for DeviceItem {deviceItem.Name}")
+    services: list[tuple[Siemens.Engineering.HW.Features.NetworkInterface, Siemens.Engineering.HW.DeviceItem]] = find_network_interface_of_device(imports, device)
+    for service in services:
+        device_item: Siemens.Engineering.HW.DeviceItem = service[0]
+        network_service: Siemens.Engineering.HW.Features.NetworkInterface = service[1]
 
         if type(network_service) is SE.HW.Features.NetworkInterface:
             node: Siemens.Engineeering.HW.Node = network_service.Nodes[0]
             node.SetAttribute("Address", device_data['network_address'])
 
-            logging.info(f"Added a network address: {device_data['network_address']}")
+            logging.info(f"Network address of {device.Name} has been set to '{node.GetAttribute("Address")}' through {device_item.Name}")
 
             interfaces.append(network_service)
 
     return interfaces
+
+
+
+
+
