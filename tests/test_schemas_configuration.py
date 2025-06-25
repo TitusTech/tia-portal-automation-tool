7704da1c9db3268626dc57e4bde4ea1ff559797e
wine-ff2 <wineffg@titusgt.com>
2025-06-25 15:40:19 +0800

    plc data types schemas test case

--- tests/test_schemas_configuration.py
+++ tests/test_schemas_configuration.py
@@ -10,6 +10,7 @@ one_device = BASE_DIR / "configs" / "one_device.json"
 one_device_with_local_modules = BASE_DIR / "configs" / "one_device_with_local_modules.json"
 multiple_devices = BASE_DIR / "configs" / "multiple_devices.json"
 multiple_devices_with_libraries = BASE_DIR / "configs" / "multiple_devices_with_libraries.json"
+multiple_devices_with_plc_data_types = BASE_DIR / "configs" / "multiple_devices_with_plc_data_types.json"
 
 def test_json_config():
     with open(one_device) as file:
@@ -24,6 +25,6 @@ def test_json_config():
         config = json.load(file)
         assert configuration.validate(config) is not None
 
-    with open(multiple_devices_with_libraries) as file:
+    with open(multiple_devices_with_plc_data_types) as file:
         config = json.load(file)
         assert configuration.validate(config) is not None
