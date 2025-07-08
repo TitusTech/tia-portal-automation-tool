4a3a9c5a70872a43dd4fd326e45f8c381e7cdc4e
wine-ff2 <wineffg@titusgt.com>
2025-07-08 18:58:26 +0800

    rename plc program blocks

--- tests/test_modules_program_blocks.py
+++ tests/test_modules_program_blocks.py
@@ -3,8 +3,8 @@ import json
 import pytest
 import xml.etree.ElementTree as ET
 
-from src.modules.FunctionBlocks import FB, FunctionBlock
-from src.modules.OrganizationBlocks import OB, OrganizationBlock, EventClassEnum
+from src.modules.BlocksFB import FB, FunctionBlock
+from src.modules.BlocksOB import OB, OrganizationBlock, EventClassEnum
 from src.modules.XML.ProgramBlocks import NetworkSource
 
 def test_organization_block():
